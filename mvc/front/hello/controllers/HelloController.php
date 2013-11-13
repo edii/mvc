594f1183f6b8c273351462527dd8e574f24bc197
schur.maksim <schur.maksim@gmail.com>
2013-11-13 13:18:13 +0200

    clear HelloControler

--- mvc/front/hello/controllers/HelloController.php
+++ mvc/front/hello/controllers/HelloController.php
@@ -4,35 +4,8 @@ class HelloController extends \Controller
 {
 	public $layout = 'column1'; //'column1'
 
-	/**
-	 * @var CActiveRecord the currently loaded data model instance.
-	 */
 	private $_model;
 
-	/**
-	 * @return array action filters
-	 */
-        
-        //function run() {
-            // set layout
-            //if($this->layout) {
-             // \init::app()->setTheme( $this->layout );
-           // }
-            // end
-            //parent::__construct();
-        //}
-        
-        
-	public function filters()
-	{
-		
-	}
-
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
 	public function actionDB()
 	{
             
@@ -55,11 +28,6 @@ class HelloController extends \Controller
              $_connector = $_db->getConnection();
              $_dbdefionitions = $_db->getDatabaseDefinition();
              
-             
-             //echo "<pre>";
-             //var_dump( $_dbdefionitions );
-             //echo "</pre>";
-             
              $query_res = $_connector -> query("SELECT * FROM section", $args, $options)-> fetchAll();
              //$query_res = $_connector -> select('section', 's', $options) 
                                // -> fields('s', array('SectionID')) 
@@ -67,16 +35,6 @@ class HelloController extends \Controller
                                // -> addTag('section_access')    
                                 //-> execute()
                                // -> fetchObject();
-             
-            //echo "<prte>";
-            //var_dump( $_connector );
-            //echo "</pre>";
-            
-            //echo "<hr /> res";
-            
-            //echo "<pre>";
-            // var_dump( $query_res );
-            //echo "</pre>";
              
              $data = ['blaaaa'];
              $this->render('db', array(
@@ -102,64 +60,6 @@ class HelloController extends \Controller
 		));  
 	}
 
-	/**
-	 * Displays a particular model.
-	 */
-	public function actionView()
-	{
-               
-		//$post=$this->loadModel();
-		//$comment=$this->newComment($post);
-
-		//$this->render('view',array(
-		//	'model'=>$post,
-		//	'comment'=>$comment,
-		//));
-	}
-
-	
-
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionUpdate()
-	{
-//		$model=$this->loadModel();
-//		if(isset($_POST['Post']))
-//		{
-//			$model->attributes=$_POST['Post'];
-//			if($model->save())
-//				$this->redirect(array('view','id'=>$model->id));
-//		}
-//
-//		$this->render('update',array(
-//			'model'=>$model,
-//		));
-	}
-
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'index' page.
-	 */
-	public function actionDelete()
-	{
-//		if(Yii::app()->request->isPostRequest)
-//		{
-//			// we only allow deletion via POST request
-//			$this->loadModel()->delete();
-//
-//			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-//			if(!isset($_GET['ajax']))
-//				$this->redirect(array('index'));
-//		}
-//		else
-//			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-	}
-
-	/**
-	 * Lists all models.
-	 */
 	public function actionIndex()
 	{
            // if($this->layout) {
@@ -167,30 +67,11 @@ class HelloController extends \Controller
            // }
             
              echo "layout ---- = ".$this->layout;
-             
              echo "<hr />";
             
-           // \init::app()->setTheme( $this->layout );
-            
             $themes = \init::app()->getTheme();
-            
-            echo "<pre>";
-            var_dump( $themes );
-            echo "</pre>";
-            
-           // echo "<hr />";
-            
-            
-            
-            // end
-            
-            
-            //echo "action Index";
-            
+                   
             $_gets = $this->getActionParams();
-            ///echo "<pre>";
-            //var_dump( $_gets );
-            //echo "</rpe>";
             
             $dataProvider = ['blaaa', 'ddddd'];
             
@@ -198,91 +79,5 @@ class HelloController extends \Controller
             $this->render('index', array(
 			'dataProvider'=>$dataProvider,
 		));
-            
-            
-            
-//		$criteria=new CDbCriteria(array(
-//			'condition'=>'status='.Post::STATUS_PUBLISHED,
-//			'order'=>'update_time DESC',
-//			'with'=>'commentCount',
-//		));
-//		if(isset($_GET['tag']))
-//			$criteria->addSearchCondition('tags',$_GET['tag']);
-//
-//		$dataProvider=new CActiveDataProvider('Post', array(
-//			'pagination'=>array(
-//				'pageSize'=>Yii::app()->params['postsPerPage'],
-//			),
-//			'criteria'=>$criteria,
-//		));
-//
-//		$this->render('index',array(
-//			'dataProvider'=>$dataProvider,
-//		));
-	}
-
-	
-	/**
-	 * Suggests tags based on the current user input.
-	 * This is called via AJAX when the user is entering the tags input.
-	 */
-	public function actionSuggestTags() {
-//		if(isset($_GET['q']) && ($keyword=trim($_GET['q']))!=='')
-//		{
-//			$tags=Tag::model()->suggestTags($keyword);
-//			if($tags!==array())
-//				echo implode("\n",$tags);
-//		}
-	}
-
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 */
-	public function loadModel()
-	{
-//		if($this->_model===null)
-//		{
-//			if(isset($_GET['id']))
-//			{
-//				if(Yii::app()->user->isGuest)
-//					$condition='status='.Post::STATUS_PUBLISHED.' OR status='.Post::STATUS_ARCHIVED;
-//				else
-//					$condition='';
-//				$this->_model=Post::model()->findByPk($_GET['id'], $condition);
-//			}
-//			if($this->_model===null)
-//				throw new CHttpException(404,'The requested page does not exist.');
-//		}
-//		return $this->_model;
-	}
-
-	/**
-	 * Creates a new comment.
-	 * This method attempts to create a new comment based on the user input.
-	 * If the comment is successfully created, the browser will be redirected
-	 * to show the created comment.
-	 * @param Post the post that the new comment belongs to
-	 * @return Comment the comment instance
-	 */
-	protected function newComment($post)
-	{
-//		$comment=new Comment;
-//		if(isset($_POST['ajax']) && $_POST['ajax']==='comment-form')
-//		{
-//			echo CActiveForm::validate($comment);
-//			Yii::app()->end();
-//		}
-//		if(isset($_POST['Comment']))
-//		{
-//			$comment->attributes=$_POST['Comment'];
-//			if($post->addComment($comment))
-//			{
-//				if($comment->status==Comment::STATUS_PENDING)
-//					Yii::app()->user->setFlash('commentSubmitted','Thank you for your comment. Your comment will be posted once it is approved.');
-//				$this->refresh();
-//			}
-//		}
-//		return $comment;
 	}
 }
