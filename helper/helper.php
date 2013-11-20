0b4c8ff25e35faf8f65a590e850a44f9950f660e
edii <edii87shadow@gmail.com>
2013-11-20 12:52:22 +0200

    helper ( n - connected classes )

---
+++ helper/helper.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of helper
+ *
+ * @author Shadow
+ */
+class helper {
+    //put your code here
+}
+
+?>
