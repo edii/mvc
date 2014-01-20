19e1abebb610a89f092efc84b159fbeaaf6afda7
edii <edii87shadow@gmail.com>
2014-01-20 22:40:29 +0200

    fix tree and owners

--- mvc/admin/tree/view/tree.php
+++ mvc/admin/tree/view/tree.php
@@ -1,21 +1,8 @@
-<?php
-$_class = (!isset($parent) and !$parent) ? 'sub': '';
-
-//    echo "<pre>";
-//    var_dump( $tree );
-//    echo "</pre>";
-?>
-<?php if(is_array($tree) and count($tree) > 0 and $validate): ?>
-<div class="sidenav">
-
-    <div class="sidebar-widget" style="margin: -1px 0 0 0;">
-        <h5 class="title" style="margin-bottom:0">Navigation</h5>
-    </div><!-- End .sidenav-widget -->
-
-    <div class="mainnav">
-        <ul>
+<?php if(is_array($tree) and count($tree) > 0 and $validate ): ?>
+        <ul class="sub">
             <?php foreach ($tree as $_items): ?>
                 <li>
+                    
                     <div class="section-item">
                         <a href="/<?= _request_uri ?>/<?= $_items['SectionUrl'] ?>">
                             <span class="icon16 icomoon-icon-stats-up"></span>
@@ -25,14 +12,12 @@ $_class = (!isset($parent) and !$parent) ? 'sub': '';
                             <span class="notification red section-item-control">sub<?= count($_items['childs']) ?></span> 
                         <?php } ?> 
                     </div>
+                    
                     <?php if(is_array($_items['childs']) and count($_items['childs']) > 0): 
                             $this->render('tree', array('tree' => $_items['childs'], 'validate' => $validate));
                          endif; 
                      ?>
                 </li>
             <?php endforeach; ?>
-            
         </ul>
-    </div>
-</div><!-- End sidenav -->
 <?php endif; ?>
