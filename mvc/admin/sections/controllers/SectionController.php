d871677b77b95fb46fd2800a426b3ea3ac22f9a6
schur.maksim <antikuklus@gmail.com>
2014-01-22 11:59:49 +0200

    add SectionsControler

---
+++ mvc/admin/sections/controllers/SectionController.php
@@ -0,0 +1,29 @@
+<?php
+
+class SectionController extends \Controller
+{
+	public $layout = 'dashboard';
+
+	private $_mauth;
+        
+        // section (model)
+        private $_msection = false;
+        /**
+         * construct
+         */
+        public function init() {
+           $this -> _mauth = \init::app() -> getModels('auth/users');
+           if(empty($this -> _msection))
+               $this -> _msection = \init::app() -> getModels('section/msection');
+        }
+        
+        public function actionIndex() {
+            $this->layout( false );
+            
+            $this->render('index', array(
+                'listing'   => $this -> _msection -> getSections(),
+                'validate'  => $this -> _mauth -> getRight(),
+                '_session'  =>  $this -> _mauth -> getValidate() -> getSession()
+            ));
+	}
+}
