01a145347f2a30f48263be772e8fbb533ec330c2
schur.maksim <antikuklus@gmail.com>
2014-01-23 14:46:16 +0200

    fix section model, controller, view
    INSERT INTO `mvc`.`section` (`SectionID`, `hidden`, `SectionInMenu`, `SectionAlias`, `OwnerID`, `UserID`, `TimeCreated`, `TimeSaved`, `SectionType`, `SectionLanguages`, `SectionParentID`, `SectionGroupID`, `SectionLayout`, `SectionBox`, `SectionBoxStyle`, `AccessGroups`, `SectionArguments`, `SectionLink`, `SectionTarget`, `SectionName`, `SectionTitle`, `SectionDescription`, `SectionKeywords`, `SectionPosition`, `SectionIntroContent`, `SectionContent`, `SectionClicks`, `SectionViewOptions`, `SectionController`, `SectionAction`, `SectionView`, `SectionUrl`, `SectionRout`) VALUES (NULL, '0', '0', 'manager_s', 'root', 'root', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'admin', '', '12', '', '', '', '', NULL, NULL, NULL, NULL, 'Sections manager (Edit/Add)', NULL, NULL, NULL, NULL, '', '', '0', '', 'section', 'manager_s', '', 'section/manager_s', '');

--- mvc/admin/section/controllers/SectionController.php
+++ mvc/admin/section/controllers/SectionController.php
@@ -27,4 +27,57 @@ class SectionController extends \Controller
                 '_session'  =>  $this -> _users -> getValidate() -> getSession()
             ));
 	}
+        
+        public function actionManager_s() {
+            $this->layout( false );
+            
+            $_error = false;
+            $_id = \init::app() ->getRequest() -> getParam('id'); 
+            $_method = \init::app() ->getRequest() -> getParam('method');
+            $_section = \init::app() ->getRequest() -> getParam('sections');
+            if(empty($_method) or !isset($_method)) {
+                // fatal error ( rediract listings owners )
+               $_error = true;
+            }
+            
+            if($_method == 'edit') {
+                 $_title = 'Редактирование';
+                 
+                 if(!(int)$_id) {
+                    $_error = true;
+                 } else {
+                     if(is_array($_section) and count($_section) > 0) {
+                        $this->_msection ->save(true, $_section);
+                     }
+                 }
+                 
+            } else if($_method == 'add'){
+                // add
+                $_title = 'Добавить';
+                if(!(int)$_id) {
+                    if(is_array($_section) and count($_section) > 0) {
+                        $this->_msection ->save(true, $_section);
+                     }
+                }
+                
+            } else {
+                $_error = true;
+            }
+            
+            // update info
+           
+               
+            if(!$_error) {   
+                $this->render('form',array(
+                    'title'   => $_title,
+                    'sections_actual' => \init::app()->getTreeSection(),
+                    'listing'   => $this->_msection -> getSectionID($_id),
+                    'validate'  => $this -> _users -> getRight(),
+                    '_session'  =>  $this -> _users -> getValidate() -> getSession()
+                ));
+            } else {
+                 $this ->redirect('/'._request_uri.'/error/404/');
+            }                 
+
+	}
 }
