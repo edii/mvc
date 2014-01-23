ca540f9242fbdbd9e8a09272df9edd5b4676c95e
edii <edii87shadow@gmail.com>
2014-01-23 14:41:21 +0200

    settings controllers!

--- mvc/admin/settings/controllers/SettingsController.php
+++ mvc/admin/settings/controllers/SettingsController.php
@@ -49,25 +49,6 @@ class SettingsController extends \Controller
              ));
 	}
         
-        function ConvertBytes($number)
-        {
-            $len = strlen($number);
-            if($len < 4)
-            {
-                return sprintf("%d b", $number);
-            }
-            if($len >= 4 && $len <=6)
-            {
-                return sprintf("%0.2f Kb", $number/1024);
-            }
-            if($len >= 7 && $len <=9)
-            {
-                return sprintf("%0.2f Mb", $number/1024/1024);
-            }
-
-            return sprintf("%0.2f Gb", $number/1024/1024/1024);
-
-        }
         
         /*
          * (sidebar left) stats-widget
