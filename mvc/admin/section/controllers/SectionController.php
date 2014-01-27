eb59696494f4e599ce68595666b40f5e1fa4594d
edii <edii87shadow@gmail.com>
2014-01-27 15:36:16 +0200

    fix section!

--- mvc/admin/section/controllers/SectionController.php
+++ mvc/admin/section/controllers/SectionController.php
@@ -19,7 +19,7 @@ class SectionController extends \Controller
         }
         
         public function actionIndex() {
-            
+           
             $this->layout( false );
             
             $_items = array();
