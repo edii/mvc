05b0a38c368d053e88b7d1098ebac70d92338245
schur.maksim <antikuklus@gmail.com>
2014-02-27 10:49:46 +0200

    fix add user form

--- mvc/admin/users/view/form.php
+++ mvc/admin/users/view/form.php
@@ -8,7 +8,7 @@
 
         <div class="heading">
 
-            <h3>Dashboard</h3>                    
+            <h3>Створення користувача</h3>                    
 
             <div class="resBtnSearch">
                 <a href="#"><span class="icon16 icomoon-icon-search-3"></span></a>
@@ -33,7 +33,7 @@
                         <span class="icon16 icomoon-icon-arrow-right-3"></span>
                     </span>
                 </li>
-                <li class="active">Dashboard</li>
+                <li class="active">Створення користувача</li>
             </ul>
 
         </div><!-- End .heading-->
@@ -56,20 +56,19 @@
                             <input type="hidden" name="method" value="<?= ($this->getParam('method') == 'edit') ? 'edit': 'add' ?>" />
                             <!-- edit -->
                             <?php if((int)$this->getParam('id')): ?>
-                                <input type="hidden" name="owner[OwnerID]" value="<?= $this->getParam('id') ?>" />
-                                <input type="hidden" name="owner[TimeSaved]" value="<?= date('Y-m-d H:i:s') ?>" />
+                                <input type="hidden" name="users[userID]" value="<?= $this->getParam('id') ?>" />
+                                <input type="hidden" name="users[TimeSaved]" value="<?= date('Y-m-d H:i:s') ?>" />
                             <?php endif; ?>
                             <!-- add -->
                             <?php if($this->getParam('method') == 'add'): ?>
-                                <input type="hidden" name="owner[TimeCreated]" value="<?= date('Y-m-d H:i:s') ?>" />
+                                <input type="hidden" name="users[TimeCreated]" value="<?= date('Y-m-d H:i:s') ?>" />
                             <?php endif; ?>     
-                                
-                            
+
                             <div class="form-row row-fluid">
                                 <div class="span12">
                                     <div class="row-fluid">
-                                        <label class="form-label span4" for="normal">OwnerName</label>
-                                        <input class="span8" id="normalInput" type="text" name="owner[OwnerName]" value="<?= (isset($listing)) ? $listing['OwnerName'] : '' ?>" />
+                                        <label class="form-label span4" for="normal">OwnerID</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[OwnerID]" value="<?= (isset($listing)) ? $listing['OwnerID'] : '' ?>" />
                                     </div>
                                 </div>
                             </div>
@@ -77,8 +76,17 @@
                             <div class="form-row row-fluid">
                                 <div class="span12">
                                     <div class="row-fluid">
-                                        <label class="form-label span4" for="normal">OwnerTitle</label>
-                                        <input class="span8" id="normalInput" type="text" name="owner[OwnerTitle]" value="<?= (isset($listing)) ? $listing['OwnerTitle'] : '' ?>" />
+                                        <label class="form-label span4" for="normal">adminID</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[adminID]" value="<?= (isset($listing)) ? $listing['adminID'] : '' ?>" />
+                                    </div>
+                                </div>
+                            </div> 
+                                
+                            <div class="form-row row-fluid">
+                                <div class="span12">
+                                    <div class="row-fluid">
+                                        <label class="form-label span4" for="normal">Ім'я</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[userName]" value="<?= (isset($listing)) ? $listing['userName'] : '' ?>" />
                                     </div>
                                 </div>
                             </div>    
@@ -86,8 +94,8 @@
                             <div class="form-row row-fluid">
                                 <div class="span12">
                                     <div class="row-fluid">
-                                        <label class="form-label span4" for="normal">OwnerDescription</label>
-                                        <input class="span8" id="normalInput" type="text" name="owner[OwnerDescription]" value="<?= (isset($listing)) ? $listing['OwnerDescription'] : '' ?>" />
+                                        <label class="form-label span4" for="normal">Логін</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[login]" value="<?= (isset($listing)) ? $listing['login'] : '' ?>" />
                                     </div>
                                 </div>
                             </div> 
@@ -96,11 +104,38 @@
                             <div class="form-row row-fluid">
                                 <div class="span12">
                                     <div class="row-fluid">
-                                        <label class="form-label span4" for="normal">OwnerKeywords</label>
-                                        <input class="span8" id="normalInput" type="text" name="owner[OwnerKeywords]" value="<?= (isset($listing)) ? $listing['OwnerKeywords'] : '' ?>" />
+                                        <label class="form-label span4" for="normal">Email</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[email]" value="<?= (isset($listing)) ? $listing['email'] : '' ?>" />
                                     </div>
                                 </div>
-                            </div>     
+                            </div>
+                                
+                            <div class="form-row row-fluid">
+                                <div class="span12">
+                                    <div class="row-fluid">
+                                        <label class="form-label span4" for="normal">userFields</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[userFields]" value="<?= (isset($listing)) ? $listing['userFields'] : '' ?>" />
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <div class="form-row row-fluid">
+                                <div class="span12">
+                                    <div class="row-fluid">
+                                        <label class="form-label span4" for="normal">userParentID</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[userParentID]" value="<?= (isset($listing)) ? $listing['userParentID'] : '' ?>" />
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <div class="form-row row-fluid">
+                                <div class="span12">
+                                    <div class="row-fluid">
+                                        <label class="form-label span4" for="normal">userLanguage</label>
+                                        <input class="span8" id="normalInput" type="text" name="users[userLanguage]" value="<?= (isset($listing)) ? $listing['userLanguage'] : '' ?>" />
+                                    </div>
+                                </div>
+                            </div>
    
                                 
                             
