0a4f711b7c6cede6ae748aed3852f9da990bf1e4
NSergey <n.sergey@dizboard.com>
2015-05-11 17:29:17 +0300

    fix delete comment in scripts!

--- mvc/admin/section/view/childs.php
+++ mvc/admin/section/view/childs.php
@@ -1,17 +1,7 @@
 <?php if(is_array($childs_list) and count($childs_list) > 0): ?>
     <ul class="padding-null box-list hidden">
-<!--   <ul class="sortable subcat-section section hidden">-->
     <?php 
-    
-//    echo "<pre>";
-//    var_dump( $childs_list );
-//    echo "</pre>"; die('stop');
-    
     $lavel ++;
-//    $_nbsp = '';
-//    for($i = 0; $i < $lavel; $i++) {
-//        $_nbsp .= '<div class="iteration">-</div>';
-//    }
     foreach($childs_list as $_key => $_item): ?>
         <li>
             <div class='list-item'>
@@ -43,28 +33,6 @@
               <?php endif; ?>
         </li>
         
-
-<!--        <tr data-id="<?= $_item['SectionParentID'] ?>_<?= $lavel ?>" class="odd gradeX hidden">
-            <td><span><?php echo str_repeat('-', $lavel); ?></span> <?= $_item['SectionID'] ?>/lv:<a class="item-edit tabledrag" href="#"><?= $lavel ?></a></td>
-            <td><?= $_item['UserID'] ?></td>
-            <td><?= $_item['TimeCreated'] ?></td>
-            <td><?= $_item['SectionName'] ?></td>
-            <td><?= $_item['SectionType'] ?></td>
-            <td><?= $_item['SectionController'] ?>/<?= $_item['SectionAction'] ?></td>
-            <td><input type="checkbox" name="checkbox" value="1" class="styled" /></td>
-            <td>
-                <div class="controls center">
-                    <a target="_blank"
-                        href="<?= $this->_getUrl() ?>/manager/method/edit/id/<?= $_item['SectionID'] ?>" 
-                       title="Редактировать Section" class="tip"><span class="icon12 icomoon-icon-pencil"></span></a>
-                    <a target="_blank"
-                        class="delete" href="<?= $this->_getUrl() ?>/delete/id/<?= $_item['SectionID'] ?>" title="Удалить Section" class="tip"><span class="icon12 icomoon-icon-remove"></span></a>
-                </div>
-            </td>
-        </tr>                                    -->
-
-      
-
     <?php endforeach; ?>
    </ul> 
   <?php endif; ?> 
